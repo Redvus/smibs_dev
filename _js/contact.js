35fdf1a3cc37efd3ad64d49ea4817747d8554b91
Redvus <a.suvorof@gmail.com>
2020-04-23 15:27:34 +0400

    Написал дополнение для виртуальной справки, чтобы ответы открывались по клику «читать дальше»

--- _js/contact.js
+++ _js/contact.js
@@ -23,13 +23,14 @@
 				ease: Power2.easeInOut
 			}, "-=0.6");
 
+		/*jshint -W030 */
 		mapButtonMobile.on('click', function () {
 			tl.reversed() ? tl.restart() : tl.reverse();
 		});
 	}
 
 	/*----------  Input mask  ----------*/
-	$('[name = name]').inputmask("a{1,64}");
+	// $('[name = name]').inputmask("a{1,64}");
 	$('[name = phone]').inputmask("+7 (999) 999 99 99");
 	// $('#af_email').inputmask("*{1,64}@*{1,64}.*{1,3}");
 
@@ -44,12 +45,46 @@
 		}
 	});
 
+	/*----------  Input mask Virtual  ----------*/
+	// $('[name = user_name], [name = subject]').inputmask("a{1,64}");
+	$('[name = user_contacts]').inputmask("+7 (999) 999 99 99");
+	// $('#af_email').inputmask("*{1,64}@*{1,64}.*{1,3}");
+
+	$('[name = user_email]').inputmask({
+		mask: "*{1,64}@a{1,64}[.a{1,3}]",
+		definitions: {
+			'*': {
+				validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+				cardinality: 1,
+				casing: "lower"
+			}
+		}
+	});
+
+	/*----------  Virtual Button  ----------*/
+	var virtualBlockMessage = $('.main-content__guest'),
+		virtualBlockButton = $('.main-content__guest-open')
+	;
+
+	virtualBlockMessage.each(function() {
+		var virtualBlockReply = $(this).find('.main-content__guest-reply'),
+			virtualBlockMask = $(this).find('.main-content__guest-mask')
+		;
+
+		$(this).find('.main-content__guest-open').on('click', function() {
+			virtualBlockReply.toggleClass('main-content__guest-reply--open');
+			virtualBlockMask.toggleClass('main-content__guest-mask--open');
+			var toggleText = $(this).data('toggle-text');
+			$(this).data('toggle-text', $(this).text()).text(toggleText);
+		});
+	});
+
 	function initPage() {
 
 	}
 
 	function initPageMobile() {
-		mapOpenMobile();
+
 	}
 
 	if (document.body.clientWidth > 768 || screen.width > 768) {
